3b067e3d25d76a98a11c0b3a3e70d62790ba8a65
sesilianovia <sesilianoviia23@gmail.com>
2024-06-21 04:52:45 +0000

    first commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('kategori'); // Ganti 'kategori' dengan route yang sesuai
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->withInput($request->only('email'));
+    }
+
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\BarangMasukController;
+use App\Http\Controllers\BarangKeluarController;
+
+
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DemoController;
+use App\Http\Controllers\SearchController;
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::resource('/products', \App\Http\Controllers\ProductController::class);
+
+Route::get('/', function () {
+    return view('login');
+});
+
+#Route::get('/', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+
+#Route::resource('barang', BarangController::class)->middleware('auth');
+Route::resource('barang', BarangController::class)->middleware('auth');
+Route::resource('kategori', KategoriController::class)->middleware('auth');
+
+Route::resource('barangmasuk', BarangMasukController::class)->middleware('auth');
+Route::resource('barangkeluar', BarangKeluarController::class)->middleware('auth');
+
+Route::get('login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('login', [LoginController::class,'authenticate']);
+
+Route::post('logout', [LoginController::class,'logout']);
+#Route::get('logout', [LoginController::class,'logout']);
+
+
+Route::get('register', [RegisterController::class,'create']);
+Route::post('register', [RegisterController::class,'store']);
+
+
+#Route::get('/dashboard', [DashboardController::class,'index']);
+
+Route::get('/demo1',[DemoController::class,'demo1']);
+// Route::get('/hello',[DemoController::class,'hello']);
+
+Route::get('/sija', function () {
+    return"Produk Kreatif dan Kewirausahaan";
+})->name('pkk');
+
+
+Route::get('/search', [SearchController::class, 'index'])->name('search');
